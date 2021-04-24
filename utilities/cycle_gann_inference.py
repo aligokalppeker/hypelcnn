093e4fb0e59abb94f37d8255b18677859da84f39
AGP-PC\AliGökalp <aligokalppeker@gmail.com>
2021-04-24 22:43:45 +0300

    Inference plot is added, more info on mean ratio Refactoring

--- utilities/cycle_gann_inference.py
+++ utilities/cycle_gann_inference.py
@@ -5,9 +5,12 @@ from __future__ import print_function
 import distutils
 import random
 
+import matplotlib.pyplot as plt
 import numpy
 import tensorflow as tf
 from absl import flags
+from numpy import linspace
+from tqdm import tqdm
 
 from common_nn_operations import get_class
 from shadow_data_generator import construct_inference_graph, model_forward_generator_name, \
@@ -20,6 +23,7 @@ else:
     pass
 
 flags.DEFINE_integer('neighborhood', 0, 'Neighborhood of samples.')
+flags.DEFINE_integer('number_of_samples', 6000, 'Number of samples.')
 flags.DEFINE_string('checkpoint_path', '',
                     'CycleGAN checkpoint path created by cycle_gann_train.py. '
                     '(e.g. "/mylogdir/model.ckpt-18442")')
@@ -68,21 +72,24 @@ def main(_):
                                                         clip_invalid_values=False)
 
     # print_tensors_in_checkpoint_file(FLAGS.checkpoint_path, tensor_name='ModelX2Y', all_tensors=True)
+    band_size = element_size[2]
+    shadow_map, shadow_ratio = loader.load_shadow_map(neighborhood, data_set)
+    shadow_ratio = shadow_ratio[0:band_size]
+    iteration_count = FLAGS.number_of_samples
 
+    # neighborhood aware indice finder
+    if neighborhood > 0:
+        indices = numpy.where(shadow_map[neighborhood:-neighborhood, neighborhood:-neighborhood] == 0)
+    else:
+        indices = numpy.where(shadow_map == 0)
+
+    progress_bar = tqdm(total=iteration_count)
     with tf.Session() as sess:
         create_generator_restorer().restore(sess, FLAGS.checkpoint_path)
 
-        shadow_map, shadow_ratio = loader.load_shadow_map(neighborhood, data_set)
-        # neighborhood aware indice finder
-        if neighborhood > 0:
-            indices = numpy.where(shadow_map[neighborhood:-neighborhood, neighborhood:-neighborhood] == 0)
-        else:
-            indices = numpy.where(shadow_map == 0)
-
-        iteration_count = 3000 * 2
-        band_size = element_size[2]
-        total_band_ratio = numpy.zeros([1, 1, band_size], dtype=float)
-        for i in range(0, iteration_count):
+        total_band_ratio = numpy.zeros([iteration_count, band_size], dtype=float)
+        inf_nan_value_count = 0
+        for index in range(0, iteration_count):
             # Pick a random point
             data_indice = random.randint(0, indices[0].size - 1)
 
@@ -96,22 +103,26 @@ def main(_):
             generated_x_data = export(sess, images_y_hwc_pl, generated_y_data, generated_x)
 
             band_ratio = numpy.mean(generated_y_data / test_x_data, axis=(0, 1))
-            shadow_calc_ratio = band_ratio * shadow_ratio[0:band_size]
 
             is_there_inf = numpy.any(numpy.isinf(band_ratio))
             is_there_nan = numpy.any(numpy.isnan(band_ratio))
             if is_there_inf or is_there_nan:
-                print("inf or nan value")
+                inf_nan_value_count = inf_nan_value_count + 1
             else:
-                total_band_ratio = total_band_ratio + band_ratio
+                total_band_ratio[index] = band_ratio
 
             if iteration_count == 1:
-                print_info(test_x_data, generated_x_data, generated_y_data, band_ratio, shadow_calc_ratio)
+                print_info(test_x_data, generated_x_data, generated_y_data, band_ratio, band_ratio * shadow_ratio)
+            progress_bar.update(1)
 
-        print("Mean total ratio")
-        print(total_band_ratio / iteration_count)
-        print("Mean Generated vs Original Ratio")
-        print(total_band_ratio / iteration_count * shadow_ratio[0:band_size])
+        progress_bar.close()
+
+        mean = numpy.mean(total_band_ratio * shadow_ratio, axis=0)
+        std = numpy.std(total_band_ratio * shadow_ratio, axis=0)
+        raw_mean = numpy.mean(total_band_ratio, axis=0)
+
+        print_overall_info(band_size, inf_nan_value_count, mean, raw_mean, std)
+        plot_overall_info(band_size, mean, std)
 
         # normal_data_as_matrix, shadow_data_as_matrix = loader.get_targetbased_shadowed_normal_data(data_set,
         #                                                                                            loader,
@@ -123,6 +134,38 @@ def main(_):
         # #                                                                             shadow_map)
         # print("Target based shadow index")
         # print(1 / numpy.squeeze(numpy.mean(normal_data_as_matrix, axis=0) / numpy.mean(shadow_data_as_matrix, axis=0)))
+
+
+def plot_overall_info(band_size, mean, std):
+    bands = linspace(1, band_size, band_size, dtype=numpy.int)
+    plt.scatter(bands, mean, label='mean ratio', s=10)
+    plt.plot(bands, mean)
+    plt.fill_between(bands, mean - std, mean + std, alpha=0.2)
+    plt.legend(loc='upper left')
+    plt.title("Band ratio")
+    plt.xlabel("bands")
+    plt.ylabel("ratio")
+    plt.savefig('band_ratio.png', dpi=300, bbox_inches='tight')
+    plt.show()
+
+
+def print_overall_info(band_size, inf_nan_value_count, mean, raw_mean, std):
+    print("inf or nan value count: %index" % inf_nan_value_count)
+    print("Mean total ratio")
+    print(raw_mean)
+    print("Mean&std Generated vs Original Ratio")
+    for band_index in range(0, band_size):
+        prefix = ""
+        postfix = ""
+        end = "  "
+        if band_index == 0:
+            prefix = "[ "
+        elif band_index == band_size - 1:
+            postfix = " ]"
+
+        if band_index % 5 == 1:
+            end = '\n'
+        print("%s%2.4f\u00B1%2.2f%s" % (prefix, mean[band_index], std[band_index], postfix), end=end)
 
 
 def print_info(test_x_data, generated_x_data, generated_y_data, band_ratio, shadow_calc_ratio):
