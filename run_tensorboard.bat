58708d817bfbc99c5b7245483b2cda1a4837842b
Gokalp Peker <aligokalppeker@yahoo.com>
2021-08-04 23:03:12 +0300

    Script fix

--- run_tensorboard.bat
+++ run_tensorboard.bat
@@ -1,4 +1,6 @@
-PATH=%PATH%;C:\ProgramData\Anaconda3\envs\tensorflow;C:\ProgramData\Anaconda3\envs\tensorflow\Library\mingw-w64\bin;C:\ProgramData\Anaconda3\envs\tensorflow\Library\usr\bin;C:\ProgramData\Anaconda3\envs\tensorflow\Library\bin;C:\ProgramData\Anaconda3\envs\tensorflow\Scripts;
+set CONDA_PATH=C:\Users\Ali.Peker\Miniconda3\envs\phd
+%set CONDA_PATH=C:\ProgramData\Anaconda3\envs\tensorflow
+PATH=%PATH%;%CONDA_PATH%;%CONDA_PATH%\Library\mingw-w64\bin;%CONDA_PATH%\Library\usr\bin;%CONDA_PATH%\Library\bin;%CONDA_PATH%\Scripts;
 start http://agp-pc:6006
 @echo tensorboard --logdir=successful_logs
 tensorboard --logdir=log
