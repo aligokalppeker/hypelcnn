848ee687801edbf6dfb58bda1a980ae89b8d7c4f
Gokalp Peker <aligokalppeker@yahoo.com>
2021-09-07 21:29:12 +0300

    Run fixes

--- run_tensorboard_gan.bat
+++ run_tensorboard_gan.bat
@@ -1,3 +1,5 @@
-PATH=%PATH%;C:\ProgramData\Anaconda3\envs\tensorflow;C:\ProgramData\Anaconda3\envs\tensorflow\Library\mingw-w64\bin;C:\ProgramData\Anaconda3\envs\tensorflow\Library\usr\bin;C:\ProgramData\Anaconda3\envs\tensorflow\Library\bin;C:\ProgramData\Anaconda3\envs\tensorflow\Scripts;
-start http://agp-pc:6006
-tensorboard --logdir=utilities\log
+set CONDA_PATH=C:\Users\Ali.Peker\Miniconda3\envs\phd
+%set CONDA_PATH=C:\ProgramData\Anaconda3\envs\tensorflow
+PATH=%PATH%;%CONDA_PATH%;%CONDA_PATH%\Library\mingw-w64\bin;%CONDA_PATH%\Library\usr\bin;%CONDA_PATH%\Library\bin;%CONDA_PATH%\Scripts;
+start http://localhost:6006
+tensorboard --logdir=utilities\log --host localhost
