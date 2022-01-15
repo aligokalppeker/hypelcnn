6baf8625c3f2dbe8b206efda79920bb5b7bc2140
Gokalp Peker <aligokalppeker@yahoo.com>
2022-01-15 22:24:10 +0300

    Identity loss is added

--- utilities/cycle_gann_train.py
+++ utilities/cycle_gann_train.py
@@ -10,6 +10,10 @@ import tensorflow as tf
 from absl import flags
 from tensorflow.contrib.data import shuffle_and_repeat
 from tensorflow.python.training.monitored_session import Scaffold
+from tensorflow_gan import gan_loss
+from tensorflow_gan.python import namedtuples
+from tensorflow_gan.python.losses import tuple_losses
+from tensorflow_gan.python.train import _validate_aux_loss_weight
 
 from DataLoader import SampleSet
 from common_nn_operations import get_class, get_all_shadowed_normal_data, get_targetbased_shadowed_normal_data
@@ -69,7 +73,181 @@ flags.DEFINE_integer(
 flags.DEFINE_float('cycle_consistency_loss_weight', 10.0,
                    'The weight of cycle consistency loss')
 
+flags.DEFINE_float('identity_loss_weight', 0.5,
+                   'The weight of cycle consistency loss')
+
+flags.DEFINE_bool('use_identity_loss', True,
+                  'Whether to use identity loss during training.')
+
 FLAGS = flags.FLAGS
+
+
+class CycleGANModelWithIdentity(tfgan.CycleGANModel):
+    """An CycleGANModel contains all the pieces needed for CycleGAN training and additional outputs for calculating identity loss.
+
+    The model `model_x2y` generator F maps data set X to Y, while the model
+    `model_y2x` generator G maps data set Y to X.
+
+    See https://arxiv.org/abs/1703.10593 for more details.
+
+    Args:
+      model_x2y: A `GANModel` namedtuple whose generator maps data set X to Y.
+      model_y2x: A `GANModel` namedtuple whose generator maps data set Y to X.
+      reconstructed_x: A `Tensor` of reconstructed data X which is G(F(X)).
+      reconstructed_y: A `Tensor` of reconstructed data Y which is F(G(Y)).
+      identity_x: A `Tensor` of data X processed by generator function F which is F(X).
+      identity_y: A `Tensor` of data Y processed by generator function G which is G(Y).
+    """
+    __slots__ = ()
+    _fields = tfgan.CycleGANModel._fields + ('identity_x', 'identity_y',)
+
+
+def cyclegan_model_with_identity(
+        # Lambdas defining models.
+        generator_fn,
+        discriminator_fn,
+        # data X and Y.
+        data_x,
+        data_y,
+        # Optional scopes.
+        generator_scope='Generator',
+        discriminator_scope='Discriminator',
+        model_x2y_scope='ModelX2Y',
+        model_y2x_scope='ModelY2X',
+        # Options.
+        check_shapes=True):
+    """Returns a CycleGAN model outputs and variables.
+
+    See https://arxiv.org/abs/1703.10593 for more details.
+
+    Args:
+      generator_fn: A python lambda that takes `data_x` or `data_y` as inputs and
+        returns the outputs of the GAN generator.
+      discriminator_fn: A python lambda that takes `real_data`/`generated data`
+        and `generator_inputs`. Outputs a Tensor in the range [-inf, inf].
+      data_x: A `Tensor` of dataset X. Must be the same shape as `data_y`.
+      data_y: A `Tensor` of dataset Y. Must be the same shape as `data_x`.
+      generator_scope: Optional generator variable scope. Useful if you want to
+        reuse a subgraph that has already been created. Defaults to 'Generator'.
+      discriminator_scope: Optional discriminator variable scope. Useful if you
+        want to reuse a subgraph that has already been created. Defaults to
+        'Discriminator'.
+      model_x2y_scope: Optional variable scope for model x2y variables. Defaults
+        to 'ModelX2Y'.
+      model_y2x_scope: Optional variable scope for model y2x variables. Defaults
+        to 'ModelY2X'.
+      check_shapes: If `True`, check that generator produces Tensors that are the
+        same shape as `data_x` (`data_y`). Otherwise, skip this check.
+
+    Returns:
+      A `CycleGANModel` namedtuple.
+
+    Raises:
+      ValueError: If `check_shapes` is True and `data_x` or the generator output
+        does not have the same shape as `data_y`.
+      ValueError: If TF is executing eagerly.
+    """
+    original_model = tfgan.cyclegan_model(generator_fn=generator_fn, discriminator_fn=discriminator_fn, data_x=data_x,
+                                          data_y=data_y, generator_scope=generator_scope,
+                                          discriminator_scope=discriminator_scope, model_x2y_scope=model_x2y_scope,
+                                          model_y2x_scope=model_y2x_scope, check_shapes=check_shapes)
+
+    with tf.compat.v1.variable_scope(original_model.model_x2y.generator_scope, reuse=True):
+        identity_x = original_model.model_x2y.generator_fn(data_x)
+    with tf.compat.v1.variable_scope(original_model.model_y2x.generator_scope, reuse=True):
+        identity_y = original_model.model_y2x.generator_fn(data_y)
+
+    model_w_identity = CycleGANModelWithIdentity(model_x2y=original_model.model_x2y,
+                                                 model_y2x=original_model.model_y2x,
+                                                 reconstructed_x=original_model.reconstructed_x,
+                                                 reconstructed_y=original_model.reconstructed_y)
+    model_w_identity.identity_x = identity_x
+    model_w_identity.identity_y = identity_y
+
+    return model_w_identity
+
+
+def identity_loss(model, kwargs):
+    # Defines identity loss
+    identity_loss_x = tf.compat.v1.losses.absolute_difference(model.model_x2y.generator_inputs,
+                                                              model.identity_x)
+    identity_loss_y = tf.compat.v1.losses.absolute_difference(model.model_y2x.generator_inputs,
+                                                              model.identity_y)
+    if kwargs.get('add_summaries', True):
+        tf.compat.v1.summary.scalar('identity_loss_x', identity_loss_x)
+        tf.compat.v1.summary.scalar('identity_loss_y', identity_loss_y)
+
+    return identity_loss_x, identity_loss_y
+
+
+def cyclegan_loss_with_identity(
+        model,
+        # Loss functions.
+        generator_loss_fn=tuple_losses.least_squares_generator_loss,
+        discriminator_loss_fn=tuple_losses.least_squares_discriminator_loss,
+        # Auxiliary losses.
+        cycle_consistency_loss_fn=tuple_losses.cycle_consistency_loss,
+        cycle_consistency_loss_weight=10.0,
+        identity_loss_weight=0.5,
+        # Options
+        **kwargs):
+    """Returns the losses for a `CycleGANModel`.
+
+    See https://arxiv.org/abs/1703.10593 for more details.
+
+    Args:
+      model: A `CycleGANModel` namedtuple.
+      generator_loss_fn: The loss function on the generator. Takes a `GANModel`
+        named tuple.
+      discriminator_loss_fn: The loss function on the discriminator. Takes a
+        `GANModel` namedtuple.
+      cycle_consistency_loss_fn: The cycle consistency loss function. Takes a
+        `CycleGANModel` namedtuple.
+      cycle_consistency_loss_weight: A non-negative Python number or a scalar
+        `Tensor` indicating how much to weigh the cycle consistency loss.
+      identity_loss_weight: A non-negative Python number or a scalar
+        `Tensor` indicating how much to weigh the identity loss.
+      **kwargs: Keyword args to pass directly to `gan_loss` to construct the loss
+        for each partial model of `model`.
+
+    Returns:
+      A `CycleGANLoss` namedtuple.
+
+    Raises:
+      ValueError: If `model` is not a `CycleGANModel` namedtuple.
+    """
+    # Sanity checks.
+    if not isinstance(model, CycleGANModelWithIdentity):
+        raise ValueError(
+            '`model` must be a `CycleGANModelWithIdentity`. Instead, was %s.' % type(model))
+
+    identity_loss_x, identity_loss_y = identity_loss(model, kwargs)
+
+    # Defines cycle consistency loss.
+    cycle_consistency_loss = cycle_consistency_loss_fn(
+        model, add_summaries=kwargs.get('add_summaries', True))
+    cycle_consistency_loss_weight = _validate_aux_loss_weight(
+        cycle_consistency_loss_weight, 'cycle_consistency_loss_weight')
+
+    aux_loss = (cycle_consistency_loss_weight * cycle_consistency_loss) + (
+            identity_loss_weight * (identity_loss_x + identity_loss_y))
+
+    # Defines losses for each partial model.
+    def _partial_loss(partial_model):
+        partial_loss = gan_loss(
+            partial_model,
+            generator_loss_fn=generator_loss_fn,
+            discriminator_loss_fn=discriminator_loss_fn,
+            **kwargs)
+        return partial_loss._replace(generator_loss=partial_loss.generator_loss +
+                                                    aux_loss)
+
+    with tf.compat.v1.name_scope('cyclegan_loss_x2y'):
+        loss_x2y = _partial_loss(model.model_x2y)
+    with tf.compat.v1.name_scope('cyclegan_loss_y2x'):
+        loss_y2x = _partial_loss(model.model_y2x)
+
+    return namedtuples.CycleGANLoss(loss_x2y, loss_y2x)
 
 
 def create_dummy_shadowed_normal_data(data_set, loader):
@@ -190,21 +368,29 @@ def get_data_from_scene(data_set, loader, shadow_map):
     return normal_data_as_matrix, shadow_data_as_matrix
 
 
-def _define_model(images_x, images_y):
+def _define_model(images_x, images_y, use_identity_loss):
     """Defines a CycleGAN model that maps between images_x and images_y.
 
     Args:
       images_x: A 4D float `Tensor` of NHWC format.  Images in set X.
       images_y: A 4D float `Tensor` of NHWC format.  Images in set Y.
+      use_identity_loss: Whether to use identity loss or not
 
     Returns:
       A `CycleGANModel` namedtuple.
     """
-    cyclegan_model = tfgan.cyclegan_model(
-        generator_fn=_shadowdata_generator_model,
-        discriminator_fn=_shadowdata_discriminator_model,
-        data_x=images_x,
-        data_y=images_y)
+    if use_identity_loss:
+        cyclegan_model = cyclegan_model_with_identity(
+            generator_fn=_shadowdata_generator_model,
+            discriminator_fn=_shadowdata_discriminator_model,
+            data_x=images_x,
+            data_y=images_y)
+    else:
+        cyclegan_model = tfgan.cyclegan_model(
+            generator_fn=_shadowdata_generator_model,
+            discriminator_fn=_shadowdata_discriminator_model,
+            data_x=images_x,
+            data_y=images_y)
 
     # Add summaries for generated images.
     # tfgan.eval.add_cyclegan_image_summaries(cyclegan_model)
@@ -315,10 +501,10 @@ def main(_):
 
         # Define CycleGAN model.
         with tf.variable_scope('Model', reuse=tf.AUTO_REUSE):
-            cyclegan_model = _define_model(images_x, images_y)
+            cyclegan_model = _define_model(images_x, images_y, FLAGS.use_identity_loss)
             input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name='x')
             dummy_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name='x')
-            cyclegan_model_validation = _define_model(input_tensor, dummy_tensor)
+            cyclegan_model_validation = _define_model(input_tensor, dummy_tensor, FLAGS.use_identity_loss)
             validation_hook = ValidationHook(validation_iteration_count,
                                              validation_sample_count,
                                              log_dir,
@@ -327,13 +513,22 @@ def main(_):
                                              input_tensor,
                                              cyclegan_model_validation.model_x2y.generated_data)
 
-        # Define CycleGAN loss.
-        cyclegan_loss = tfgan.cyclegan_loss(
-            cyclegan_model,
-            # generator_loss_fn=wasserstein_generator_loss,
-            # discriminator_loss_fn=wasserstein_discriminator_loss,
-            cycle_consistency_loss_weight=FLAGS.cycle_consistency_loss_weight,
-            tensor_pool_fn=tfgan.features.tensor_pool)
+        if FLAGS.use_identity_loss:
+            cyclegan_loss = cyclegan_loss_with_identity(
+                cyclegan_model,
+                # generator_loss_fn=wasserstein_generator_loss,
+                # discriminator_loss_fn=wasserstein_discriminator_loss,
+                cycle_consistency_loss_weight=FLAGS.cycle_consistency_loss_weight,
+                identity_loss_weight=FLAGS.identity_loss_weight,
+                tensor_pool_fn=tfgan.features.tensor_pool)
+        else:
+            # Define CycleGAN loss.
+            cyclegan_loss = tfgan.cyclegan_loss(
+                cyclegan_model,
+                # generator_loss_fn=wasserstein_generator_loss,
+                # discriminator_loss_fn=wasserstein_discriminator_loss,
+                cycle_consistency_loss_weight=FLAGS.cycle_consistency_loss_weight,
+                tensor_pool_fn=tfgan.features.tensor_pool)
 
         # Define CycleGAN train ops.
         train_ops = _define_train_ops(cyclegan_model, cyclegan_loss)
