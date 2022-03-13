d7a702f1601f424c6adcdabdf854cf1117d7f0d6
Gokalp Peker <aligokalppeker@yahoo.com>
2022-03-13 20:53:53 +0300

    Integrate gan wrapper.

--- utilities/gan_wrapper.py
+++ utilities/gan_wrapper.py
@@ -10,6 +10,12 @@ from shadow_data_generator import _shadowdata_generator_model, _shadowdata_discr
 from gan_common import ValidationHook
 
 model_forward_generator_name = 'ModelX2Y'
+input_x_tensor_name = "x"
+input_y_tensor_name = "y"
+
+
+def adj_shadow_ratio(shadow_ratio, is_shadow):
+    return 1. / shadow_ratio if is_shadow else shadow_ratio
 
 
 class GANWrapper:
@@ -62,18 +68,19 @@ class GANWrapper:
         element_size = loader.get_data_shape(data_set)
         element_size = [1, element_size[0], element_size[1], element_size[2] - 1]
 
-        x_input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name='x')
-        y_input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name='y')
+        x_input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name=input_x_tensor_name)
+        y_input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name=input_y_tensor_name)
         model_for_validation = self.define_model(x_input_tensor, y_input_tensor)
         shadowed_validation_hook = ValidationHook(iteration_freq=validation_iteration_count,
                                                   sample_count=validation_sample_count,
                                                   log_dir=log_dir,
                                                   loader=loader, data_set=data_set, neighborhood=neighborhood,
                                                   shadow_map=shadow_map,
-                                                  shadow_ratio=shadow_ratio,
-                                                  input_tensor=x_input_tensor,
+                                                  shadow_ratio=adj_shadow_ratio(shadow_ratio, not self._swap_inputs),
+                                                  input_tensor=y_input_tensor if self._swap_inputs else x_input_tensor,
                                                   model=model_for_validation.generated_data,
-                                                  fetch_shadows=False, name_suffix="shadowed")
+                                                  fetch_shadows=False,
+                                                  name_suffix="deshadowed" if self._swap_inputs else "shadowed")
         return shadowed_validation_hook
 
 
@@ -111,10 +118,16 @@ class GANInferenceWrapper:
 
         return image_output_row
 
-    def make_inference_graph(self, data_set, loader, is_shadow_graph, clip_invalid_values):
+    @staticmethod
+    def __create_input_tensor(data_set, loader, is_shadow_graph):
         element_size = loader.get_data_shape(data_set)
         element_size = [1, element_size[0], element_size[1], element_size[2] - 1]
-        input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name='x')
+        input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size,
+                                      name=input_x_tensor_name if is_shadow_graph else input_y_tensor_name)
+        return input_tensor
+
+    def make_inference_graph(self, data_set, loader, is_shadow_graph, clip_invalid_values):
+        input_tensor = self.__create_input_tensor(data_set, loader, is_shadow_graph)
         generated = self.construct_inference_graph(input_tensor, is_shadow_graph, clip_invalid_values)
         return input_tensor, generated
 
@@ -128,21 +141,13 @@ class GANInferenceWrapper:
 
     def create_inference_hook(self, data_set, loader, log_dir, neighborhood, shadow_map, shadow_ratio,
                               validation_sample_count):
-        element_size = loader.get_data_shape(data_set)
-        element_size = [1, element_size[0], element_size[1], element_size[2] - 1]
-
-        if self.fetch_shadows:
-            shadow_ratio = 1. / shadow_ratio
-        else:
-            shadow_ratio = shadow_ratio
-
-        x_input_tensor = tf.placeholder(dtype=tf.float32, shape=element_size, name='x')
+        input_tensor = self.__create_input_tensor(data_set, loader, self.fetch_shadows)
         return ValidationHook(iteration_freq=0,
                               sample_count=validation_sample_count,
                               log_dir=log_dir,
                               loader=loader, data_set=data_set, neighborhood=neighborhood,
                               shadow_map=shadow_map,
-                              shadow_ratio=shadow_ratio,
-                              input_tensor=x_input_tensor,
-                              model=self.construct_inference_graph(x_input_tensor, None),
-                              fetch_shadows=self.fetch_shadows, name_suffix="shadowed")
+                              shadow_ratio=adj_shadow_ratio(shadow_ratio, self.fetch_shadows),
+                              input_tensor=input_tensor,
+                              model=self.construct_inference_graph(input_tensor, None),
+                              fetch_shadows=self.fetch_shadows, name_suffix="")
